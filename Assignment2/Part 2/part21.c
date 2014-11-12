ac7ba51ffd2ac3437698fde8f88ef60a1a6a289c
Jeton <jsinoimeri@yahoo.ca>
2014-11-11 22:47:42 -0500

    Version 1.3 of Assingment 2

--- Assignment2/Part 2/part21.c
+++ Assignment2/Part 2/part21.c
@@ -13,12 +13,17 @@
  *******************************************************/
 
 
+// throughput is the number of processors completed per time unit
+// turnaround time is the amount of time to complete a process
+// wait time is the amount of time that the process spends in the 
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <pthread.h>
 #include <sys/types.h>
-
+#include <pthread.h>
 
 
 // linked list stucture
@@ -26,13 +31,15 @@ typedef struct node
 {
   int pID;                  // value of pid to be stored
   
-  time_t arrivalTime,          // value of arrival time
+  time_t arrivalTime;       // value of arrival time
   
   int totalCPUTime,         // value of Total CPU time
       IOFrequency,          // value of I/O frequency
       IODuration;           // value of IO Duration
   
   double waitTime;          // value of wait time
+  
+  time_t completeTime;      // value of time for process to complete
   
   struct node *next;        // pointer to next node
 } Node;
@@ -41,23 +48,34 @@ typedef struct node
 
 int jobcount = 0;            // the number of jobs in the system
 
+pthread_t ThreadA;
 
 
 // Function prototypes
 void pop(Node **head, Node **item);
-void push(int pid, time_t arTime, int tCtime, int IOF, int IOD, Node **head, Node **tail);
+void push(int pid, int tCtime, int IOF, int IOD, double waitTime, double completeT, Node **head, Node **tail);
 void readFile(FILE *ptr_file, Node **head, Node **tail);
-void printList(Node *head, Node *tail);
 
 void timer(Node *pcb, int timeSpent);
 void IOExecution(Node *currentProcess, Node **head, Node **tail);
-void CPUExecution(Node **head, Node **tail);
+void CPUExecution(Node **head, Node **tail, Node **terHead, Node **terTail);
+
+void pushGivenNode(Node *t, Node **head, Node **tail);
+int LinkedListgetSize(Node *head, Node *tail);
+double calcThroughPut(time_t startTime, time_t endTime);
+
+double calcAvgWaitTime(Node *terHead);
+double calcAvgTurnAroundTime(time_t startTime, Node *terHead);
+
+
 
 
 int main(void)
 {
 	Node * headReady = NULL,            // linked list head pointer
-       * tailReady = headReady;       // linked list tail pointer
+       * tailReady = headReady,       // linked list tail pointer
+       * headTerminated = NULL,
+       * tailTerminated = headTerminated; 
 
 	FILE *ptr_file = NULL;              // File pointer
 
@@ -65,20 +83,33 @@ int main(void)
 	// read the file
 	readFile(ptr_file, &headReady, &tailReady);
 	
-	Node *item;
-	//printList(headReady, tailReady);
-	//pop(&headReady, &item);
-	//timer(item, &headReady, &tailReady);
+		time_t startSimTime,
+				 endSimTime;
+				 
+				 
+	time(&startSimTime);
 	
-	//pop(&headReady, &item);
-	//timer(item, &headReady, &tailReady);
 	
+	// start simulation
 	while(headReady != NULL)
-		CPUExecution(&headReady, &tailReady);
+		CPUExecution(&headReady, &tailReady, &headTerminated, &tailTerminated);
 	
 	
+	time(&endSimTime);
 	
 	
+	double throughPut = calcThroughPut(startSimTime, endSimTime);
+	double avgWaitTime = calcAvgWaitTime(headTerminated);
+	double avgTurnAroundTime = calcAvgTurnAroundTime(startSimTime, headTerminated);
+	
+	printf("The size of the job queue is: %i\n", LinkedListgetSize(headReady, tailReady));
+	printf("The size of the terminated queue is: %i\n", LinkedListgetSize(headTerminated, tailTerminated));
+	
+	
+	printf("The throughput is: %lf\n", throughPut);
+	printf("The avg wait time is: %lf\n", avgWaitTime);
+	printf("The avg turn around time is: %lf\n", avgTurnAroundTime);
+
 	
 	return 0;
 }
@@ -95,7 +126,7 @@ int main(void)
  *                                                   *
  *****************************************************/
 
-
+/*
 void printList(Node *head, Node *tail)
 {
 	Node *head2 = head;
@@ -113,17 +144,82 @@ void printList(Node *head, Node *tail)
 		printf("pid 3: %i Arrive_time: %i CPUTIME: %i IOFreq: %i IOdur: %i\n", head2 -> pID, head2 -> arrivalTime, head2 -> totalCPUTime, head2 -> IOFrequency, head2 -> IODuration);
 	}
 }  
+*/
+
+
+double calcThroughPut(time_t startTime, time_t endTime)
+{
+	double throughPut = jobcount / difftime(endTime, startTime);
+	
+	return throughPut;
+}
+
+
+double calcAvgWaitTime(Node *terHead)
+{
+	double totalWaitTime = 0;
+	
+	while (terHead)
+	{		
+		totalWaitTime  += terHead -> waitTime;
+		terHead = terHead -> next;
+	}
+	
+	double avgWaitTime = totalWaitTime / jobcount;
+	
+	return avgWaitTime;
+	
+}
+
+
+double calcAvgTurnAroundTime(time_t startTime, Node *terHead)
+{
+	double totalTurnAroundTime = 0;
+	
+	while (terHead)
+	{
+		totalTurnAroundTime += difftime(terHead -> completeTime, startTime);
+		terHead = terHead -> next;
+	}
+	
+	double avgTurnAroundTime = totalTurnAroundTime / jobcount;
+	
+	return avgTurnAroundTime;
+	
+}
 
 
 
-void CPUExecution(Node **head, Node **tail)
+int LinkedListgetSize(Node *head, Node *tail)
+{
+	int size = 0;
+	
+	while (head)
+	{
+		size ++;
+		head = head -> next;
+	}
+	
+	return size;
+	
+}
+
+
+void CPUExecution(Node **head, Node **tail, Node **terHead, Node **terTail)
 {
 	Node *currentProcess;
 	
+	
+	
+	
 	pop(head, &currentProcess);
 	
-	printf("The process ID: %i is entering CPUExecution\n", currentProcess -> pID);
 	
+	currentProcess -> waitTime += difftime(time(NULL), currentProcess -> arrivalTime);
+	
+	
+	printf("The process ID: %i is entering CPUExecution\n", currentProcess -> pID);
+	printf("This process waited for %lf\n", currentProcess -> waitTime);
 	
 	int ioFreq = currentProcess -> IOFrequency;
 	
@@ -135,15 +231,23 @@ void CPUExecution(Node **head, Node **tail)
 		timer(currentProcess, currentProcess -> IOFrequency);    // cpu exectution
 		
 		if (currentProcess -> totalCPUTime > 0)
-			IOExecution(currentProcess, head, tail);   // send to io device
+		{
+			IOExecution(currentProcess, head, tail);
+		}
 		
 	}
 	
 	else
 	{
 		timer(currentProcess, currentProcess -> totalCPUTime);
+		
+		currentProcess -> completeTime = time(NULL);
+		
+		pushGivenNode(currentProcess, terHead, terTail);
+		
 		printf("The process ID: %i is complete CPUExecution\n", currentProcess -> pID);
-	}
+		
+		} 
 }
 
 
@@ -152,7 +256,9 @@ void IOExecution(Node *currentProcess, Node **head, Node **tail)
 	printf("PID: %i entering IOExecution\n", currentProcess -> pID);
 	timer(currentProcess, currentProcess -> IODuration);
 		
-	push(currentProcess -> pID, currentProcess -> arrivalTime, currentProcess -> totalCPUTime, currentProcess -> IOFrequency, currentProcess -> IODuration, head, tail);
+	push(currentProcess -> pID, currentProcess -> totalCPUTime, currentProcess -> IOFrequency, currentProcess -> IODuration,
+	     currentProcess -> waitTime, currentProcess -> completeTime, head, tail);
+	     
 	printf("PID: %i leaving IOExecution\n", currentProcess -> pID);
 }
 
@@ -239,20 +345,20 @@ void pop(Node **head, Node **item)
  *                                   *
  *************************************/
 
-void push(int pid, int arTime, int tCtime, int IOF, int IOD, Node **head, Node **tail)
+void push(int pid, int tCtime, int IOF, int IOD, double waitTime, double completeT, Node **head, Node **tail)
 {
-	
-	
 	
 	// create a new node
 	Node *new_node = (Node *)malloc(sizeof(Node));
 	
 	// assign the values
 	new_node -> pID = pid;
-	new_node -> arrivalTime = arTime;         
+	new_node -> arrivalTime = time(NULL);         
   new_node -> totalCPUTime = tCtime;         
   new_node -> IOFrequency = IOF;          
-  new_node -> IODuration = IOD;          
+  new_node -> IODuration = IOD;  
+  new_node -> waitTime = waitTime;
+  new_node -> completeTime = completeT;     
 	new_node -> next = NULL;
 	
 	
@@ -272,6 +378,23 @@ void push(int pid, int arTime, int tCtime, int IOF, int IOD, Node **head, Node *
 	
 }
 
+
+void pushGivenNode(Node *t, Node **head, Node **tail)
+{	
+	// check if new node is the first in the list
+	if (!*head)
+	{
+		*head = t;
+		*tail = *head;
+	}
+	
+	// assign tail to point to the last element pointer
+	else
+	{
+		(*tail)-> next = t;
+	  *tail = t;
+  }
+}
 
 
 /**************************************
@@ -303,12 +426,11 @@ void readFile(FILE *ptr_file, Node **head, Node **tail)
 	int pid,               // value of pid to be stored
 	    IOf,               // value of I/O frequency   
 	    IOd,               // value of IO Duration 
-      arrive_time,       // value of arrival time    
       cpu_time;          // value of Total CPU time       
 	
 	
 	// open file
-	ptr_file = fopen("Input.bat","r");
+	ptr_file = fopen("Input1.bat","r");
 	
 	
 	// return if file cannot be opened
@@ -320,12 +442,10 @@ void readFile(FILE *ptr_file, Node **head, Node **tail)
 
 
 	// otherwise read, push the info into the linked list and increment the num of jobs until end of file
-  while(fscanf(ptr_file,"%i %i %i %i %i ", &pid, &arrive_time, &cpu_time, &IOf, &IOd) != EOF)
+  while(fscanf(ptr_file,"%i %i %i %i ", &pid,  &cpu_time, &IOf, &IOd) != EOF)
   {
-    push(pid,arrive_time,cpu_time,IOf,IOd, head, tail);
+    push(pid,cpu_time,IOf,IOd, 0, 0, head, tail);
     jobcount ++;
-    
-    //printf("pid 1: %i Arrive_time: %i CPUTIME: %i IOFreq: %i IOdur: %i\n",pid,arrive_time,cpu_time,IOf,IOd);
   }
   
   
--- Assignment2/Part 2/part22.c
+++ Assignment2/Part 2/part22.c
@@ -13,6 +13,11 @@
  *******************************************************/
 
 
+// throughput is the number of processors completed per time unit
+// turnaround time is the amount of time to complete a process
+// wait time is the amount of time that the process spends in the 
+
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
@@ -20,13 +25,13 @@
 #include <sys/types.h>
 
 
-
 // linked list stucture
 typedef struct node 
 {
-  int pID;                  // value of pid to be stored
+  int pID,                  // value of pid to be stored
+      arrivalTime;          // value of arrival time
   
-  time_t arrivalTime,          // value of arrival time
+  time_t readyTime;       // value of ready queue time
   
   int totalCPUTime,         // value of Total CPU time
       IOFrequency,          // value of I/O frequency
@@ -34,8 +39,19 @@ typedef struct node
   
   double waitTime;          // value of wait time
   
+  time_t completeTime;      // value of time for process to complete
+  
   struct node *next;        // pointer to next node
 } Node;
+
+
+// linked list stucture
+typedef struct args 
+{
+	struct node *current;
+  struct node **head;
+  struct node **tail;        // pointer to next node
+} Args;
 
 
 
@@ -43,21 +59,32 @@ int jobcount = 0;            // the number of jobs in the system
 
 
 
+
 // Function prototypes
 void pop(Node **head, Node **item);
-void push(int pid, time_t arTime, int tCtime, int IOF, int IOD, Node **head, Node **tail);
+void push(int pid, int tCtime, int IOF, int IOD, double waitTime, double completeT, Node **head, Node **tail);
 void readFile(FILE *ptr_file, Node **head, Node **tail);
-void printList(Node *head, Node *tail);
 
 void timer(Node *pcb, int timeSpent);
-void IOExecution(Node *currentProcess, Node **head, Node **tail);
-void CPUExecution(Node **head, Node **tail);
+void *IOExecution(void *args);
+void CPUExecution(Node **head, Node **tail, Node **terHead, Node **terTail);
+
+void pushGivenNode(Node *t, Node **head, Node **tail);
+int LinkedListgetSize(Node *head);
+double calcThroughPut(time_t startTime, time_t endTime);
+
+double calcAvgWaitTime(Node *terHead);
+double calcAvgTurnAroundTime(time_t startTime, Node *terHead);
+
+
 
 
 int main(void)
 {
 	Node * headReady = NULL,            // linked list head pointer
-       * tailReady = headReady;       // linked list tail pointer
+       * tailReady = headReady,       // linked list tail pointer
+       * headTerminated = NULL,
+       * tailTerminated = headTerminated; 
 
 	FILE *ptr_file = NULL;              // File pointer
 
@@ -65,20 +92,33 @@ int main(void)
 	// read the file
 	readFile(ptr_file, &headReady, &tailReady);
 	
-	Node *item;
-	//printList(headReady, tailReady);
-	//pop(&headReady, &item);
-	//timer(item, &headReady, &tailReady);
-	
-	//pop(&headReady, &item);
-	//timer(item, &headReady, &tailReady);
-	
-	while(headReady != NULL)
-		CPUExecution(&headReady, &tailReady);
+	time_t startSimTime,
+				 endSimTime;
+				 
+				 
+	time(&startSimTime);
 	
 	
+	// start simulation
+	while(LinkedListgetSize(headTerminated) != jobcount)
+		CPUExecution(&headReady, &tailReady, &headTerminated, &tailTerminated);
 	
 	
+	time(&endSimTime);
+	
+	
+	double throughPut = calcThroughPut(startSimTime, endSimTime);
+	double avgWaitTime = calcAvgWaitTime(headTerminated);
+	double avgTurnAroundTime = calcAvgTurnAroundTime(startSimTime, headTerminated);
+	
+	printf("The size of the job queue is: %i\n", LinkedListgetSize(headReady));
+	printf("The size of the terminated queue is: %i\n", LinkedListgetSize(headTerminated));
+	
+	
+	printf("\nThe throughput is: %lf\n", throughPut);
+	printf("The avg wait time is: %lf\n", avgWaitTime);
+	printf("The avg turn around time is: %lf\n", avgTurnAroundTime);
+
 	
 	return 0;
 }
@@ -95,7 +135,7 @@ int main(void)
  *                                                   *
  *****************************************************/
 
-
+/*
 void printList(Node *head, Node *tail)
 {
 	Node *head2 = head;
@@ -113,47 +153,135 @@ void printList(Node *head, Node *tail)
 		printf("pid 3: %i Arrive_time: %i CPUTIME: %i IOFreq: %i IOdur: %i\n", head2 -> pID, head2 -> arrivalTime, head2 -> totalCPUTime, head2 -> IOFrequency, head2 -> IODuration);
 	}
 }  
+*/
+
+
+double calcThroughPut(time_t startTime, time_t endTime)
+{
+	double throughPut = jobcount / difftime(endTime, startTime);
+	
+	return throughPut;
+}
+
+
+double calcAvgWaitTime(Node *terHead)
+{
+	double totalWaitTime = 0;
+	
+	while (terHead)
+	{		
+		totalWaitTime  += terHead -> waitTime;
+		terHead = terHead -> next;
+	}
+	
+	double avgWaitTime = totalWaitTime / jobcount;
+	
+	return avgWaitTime;
+	
+}
+
+
+double calcAvgTurnAroundTime(time_t startTime, Node *terHead)
+{
+	double totalTurnAroundTime = 0;
+	
+	while (terHead)
+	{
+		totalTurnAroundTime += difftime(terHead -> completeTime, startTime);
+		terHead = terHead -> next;
+	}
+	
+	double avgTurnAroundTime = totalTurnAroundTime / jobcount;
+	
+	return avgTurnAroundTime;
+	
+}
 
 
 
-void CPUExecution(Node **head, Node **tail)
+int LinkedListgetSize(Node *head)
+{
+	int size = 0;
+	
+	while (head)
+	{
+		size ++;
+		head = head -> next;
+	}
+	
+	return size;
+	
+}
+
+
+void CPUExecution(Node **head, Node **tail, Node **terHead, Node **terTail)
 {
 	Node *currentProcess;
 	
 	pop(head, &currentProcess);
 	
-	printf("The process ID: %i is entering CPUExecution\n", currentProcess -> pID);
-	
-	
-	int ioFreq = currentProcess -> IOFrequency;
-	
-	
-	if (currentProcess -> IOFrequency > 0 && currentProcess -> totalCPUTime > ioFreq)
+	if (currentProcess)
 	{
-		currentProcess -> totalCPUTime = currentProcess -> totalCPUTime - currentProcess -> IOFrequency;
-		
-		timer(currentProcess, currentProcess -> IOFrequency);    // cpu exectution
-		
-		if (currentProcess -> totalCPUTime > 0)
-			IOExecution(currentProcess, head, tail);   // send to io device
-		
-	}
 	
-	else
-	{
-		timer(currentProcess, currentProcess -> totalCPUTime);
-		printf("The process ID: %i is complete CPUExecution\n", currentProcess -> pID);
+		currentProcess -> waitTime += difftime(time(NULL), currentProcess -> arrivalTime);
+		
+		
+		printf("The process ID: %i is entering CPUExecution\n", currentProcess -> pID);
+		printf("This process waited for %lf\n milliseconds", currentProcess -> waitTime);
+		
+		int ioFreq = currentProcess -> IOFrequency;
+		
+		
+		if (currentProcess -> IOFrequency > 0 && currentProcess -> totalCPUTime > ioFreq)
+		{
+			currentProcess -> totalCPUTime = currentProcess -> totalCPUTime - currentProcess -> IOFrequency;
+			
+			timer(currentProcess, currentProcess -> IOFrequency);    // cpu exectution
+			
+			if (currentProcess -> totalCPUTime > 0)
+			{
+				pthread_t ThreadA;
+				
+				Args *args1 = (Args *)malloc(sizeof(Args));
+				args1 -> current = currentProcess;
+				args1 -> head = head;
+				args1 -> tail = tail;
+				
+				pthread_create(&ThreadA, NULL, IOExecution, (void *)args1);
+			}
+			
+		}
+		
+		else
+		{
+			timer(currentProcess, currentProcess -> totalCPUTime);
+			
+			currentProcess -> completeTime = time(NULL);
+			
+			pushGivenNode(currentProcess, terHead, terTail);
+			
+			printf("The process ID: %i is complete CPUExecution\n", currentProcess -> pID);
+			
+			}
 	}
 }
 
 
-void IOExecution(Node *currentProcess, Node **head, Node **tail)
+void *IOExecution(void *args)
 {
+	Args *args1 = (Args *)args;
+	Node *currentProcess = args1 -> current;
+	
+	 
 	printf("PID: %i entering IOExecution\n", currentProcess -> pID);
 	timer(currentProcess, currentProcess -> IODuration);
 		
-	push(currentProcess -> pID, currentProcess -> arrivalTime, currentProcess -> totalCPUTime, currentProcess -> IOFrequency, currentProcess -> IODuration, head, tail);
+	push(currentProcess -> pID, currentProcess -> totalCPUTime, currentProcess -> IOFrequency, currentProcess -> IODuration,
+	     currentProcess -> waitTime, currentProcess -> completeTime, args1 -> head, args1 -> tail);
+	     
 	printf("PID: %i leaving IOExecution\n", currentProcess -> pID);
+	
+	return NULL;
 }
 
 
@@ -239,20 +367,20 @@ void pop(Node **head, Node **item)
  *                                   *
  *************************************/
 
-void push(int pid, int arTime, int tCtime, int IOF, int IOD, Node **head, Node **tail)
+void push(int pid, int tCtime, int IOF, int IOD, double waitTime, double completeT, Node **head, Node **tail)
 {
-	
-	
 	
 	// create a new node
 	Node *new_node = (Node *)malloc(sizeof(Node));
 	
 	// assign the values
 	new_node -> pID = pid;
-	new_node -> arrivalTime = arTime;         
+	new_node -> arrivalTime = time(NULL);         
   new_node -> totalCPUTime = tCtime;         
   new_node -> IOFrequency = IOF;          
-  new_node -> IODuration = IOD;          
+  new_node -> IODuration = IOD;  
+  new_node -> waitTime = waitTime;
+  new_node -> completeTime = completeT;     
 	new_node -> next = NULL;
 	
 	
@@ -272,6 +400,23 @@ void push(int pid, int arTime, int tCtime, int IOF, int IOD, Node **head, Node *
 	
 }
 
+
+void pushGivenNode(Node *t, Node **head, Node **tail)
+{	
+	// check if new node is the first in the list
+	if (!*head)
+	{
+		*head = t;
+		*tail = *head;
+	}
+	
+	// assign tail to point to the last element pointer
+	else
+	{
+		(*tail)-> next = t;
+	  *tail = t;
+  }
+}
 
 
 /**************************************
@@ -303,12 +448,11 @@ void readFile(FILE *ptr_file, Node **head, Node **tail)
 	int pid,               // value of pid to be stored
 	    IOf,               // value of I/O frequency   
 	    IOd,               // value of IO Duration 
-      arrive_time,       // value of arrival time    
       cpu_time;          // value of Total CPU time       
 	
 	
 	// open file
-	ptr_file = fopen("Input.bat","r");
+	ptr_file = fopen("Input1.bat","r");
 	
 	
 	// return if file cannot be opened
@@ -320,12 +464,10 @@ void readFile(FILE *ptr_file, Node **head, Node **tail)
 
 
 	// otherwise read, push the info into the linked list and increment the num of jobs until end of file
-  while(fscanf(ptr_file,"%i %i %i %i %i ", &pid, &arrive_time, &cpu_time, &IOf, &IOd) != EOF)
+  while(fscanf(ptr_file,"%i %i %i %i ", &pid,  &cpu_time, &IOf, &IOd) != EOF)
   {
-    push(pid,arrive_time,cpu_time,IOf,IOd, head, tail);
+    push(pid,cpu_time,IOf,IOd, 0, 0, head, tail);
     jobcount ++;
-    
-    //printf("pid 1: %i Arrive_time: %i CPUTIME: %i IOFreq: %i IOdur: %i\n",pid,arrive_time,cpu_time,IOf,IOd);
   }
   
   
